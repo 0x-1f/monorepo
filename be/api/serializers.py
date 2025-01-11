182f088e06c2606450e6af2faa0494e94a345f6c
sungmiki42 <sungmiki@student.42seoul.kr>
2025-01-11 23:03:55 +0900

    feat: pong serializer removed

--- be/api/serializers.py
+++ be/api/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
-from .models import Item, PongGame
+from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
 
-class PongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PongGame
-        fields = '__all__'
+# class PongSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = PongGame
+#         fields = '__all__'
 
 # class RSPSerializer(serializers.ModelSerializer):
 #     class Meta:
