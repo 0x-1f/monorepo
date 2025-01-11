6b46e8c28d66ed9a2a9c1644b4201d982ebbaa3a
sungmiki42 <sungmiki@student.42seoul.kr>
2025-01-11 23:04:52 +0900

    feat: users viewset router에 등록

--- be/api/urls.py
+++ be/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet, PongViewSet
+from .views import ItemViewSet
 from login.views import IntraAuthViewSet
+from user.views import UsersViewSet
 
 router = DefaultRouter()
-router.register(r'pong', PongViewSet, basename="pong")
+# router.register(r'pong', PongViewSet, basename="pong")
 router.register(r'auth', IntraAuthViewSet, basename="login")
 # router.register(r'rsp', RSPViewSet, basename="rsp")
+router.register(r'users', UsersViewSet, basename="rsp")
 
 urlpatterns = [
 	path('api/', include(router.urls)),
