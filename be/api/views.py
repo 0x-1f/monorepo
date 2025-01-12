8d4f888a4f88a939c49cf711454d86e2db38651b
sungmiki42 <sungmiki@student.42seoul.kr>
2025-01-12 13:32:23 +0900

    feat: pong, rps history 검색 api 추가

--- be/api/views.py
+++ be/api/views.py
@@ -6,9 +6,14 @@ from django.conf import settings
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action, api_view
+from rest_framework import generics
+from django.db.models import Q
 
 from .models import Item
+from games.models import PongGame, RPSGame
+from user.models import Users
 from .serializers import ItemSerializer
+from games.serializers import PongSerializer, RPSSerializer
 from user.views import UsersViewSet
 from login.views import IntraAuthViewSet
 
@@ -16,11 +21,20 @@ class ItemViewSet(viewsets.ModelViewSet):
 	queryset = Item.objects.all()
 	serializer_class = ItemSerializer
 
-# class PongViewSet(viewsets.ModelViewSet):
-# 	queryset = PongGame.objects.all()
-# 	serializer_class = PongSerializer
+class PongHistoryView(generics.ListAPIView):
+    serializer_class = PongSerializer
 
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']  # URL에서 사용자 ID를 가져옵니다.
+        return PongGame.objects.filter(
+            Q(winner__user_id=user_id) | Q(loser__user_id=user_id)
+        ).order_by('created_date')
 
-# class RSPViewSet(viewsets.ModelViewSet):
-#     queryset = RSPGame.objects.all()
-#     serializer_class = RSPSerializer
+class RPSHistoryView(generics.ListAPIView):
+    serializer_class = RPSSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']  # URL에서 사용자 ID를 가져옵니다.
+        return RPSGame.objects.filter(
+            Q(player1__user_id=user_id) | Q(player2__user_id=user_id)
+        ).order_by('created_date')
