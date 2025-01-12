87b5ea8aeee287bd10fb5c0e5a3284fc8dbb5f18
Hyeungeun Kim <hyeunkim@c8r5s11.42seoul.kr>
2025-01-12 22:37:52 +0900

    rm: api에 안쓰는 view, serializer 등 정리

--- be/api/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .consumers import PongConsumer
-
-websocket_urlpatterns = [
-    path('ws/pong/<str:session_id>', PongConsumer.as_asgi()),  # WebSocket 경로 설정
-]
--- be/api/urls.py
+++ be/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+
 from login.views import IntraAuthViewSet
 from user.views import UsersViewSet
 from games.views import RPSViewSet, PongViewSet
-from .views import PongHistoryView, RPSHistoryView
 
 router = DefaultRouter()
 router.register(r'pong', PongViewSet, basename="pong")
@@ -14,6 +14,4 @@ router.register(r'users', UsersViewSet, basename="users")
 
 urlpatterns = [
 	path('api/', include(router.urls)),
-	path('api/pong/<int:user_id>/history/', PongHistoryView.as_view(), name='pong-game-history'),
-	path('api/rps/<int:user_id>/history/', RPSHistoryView.as_view(), name='rps-game-history'),
 ]
