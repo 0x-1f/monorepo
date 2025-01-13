8edc92a097fa33cb7a9e9f256616fc1a3a8af996
hyeunkim42 <147035697+hyeunkim42@users.noreply.github.com>
2025-01-13 12:11:42 +0900

    feat: add pong history serializer

--- be/games/views.py
+++ be/games/views.py
@@ -9,30 +9,47 @@ from rest_framework.decorators import action
 
 from user.models import Users
 from .models import PongGame, RPSGame
-from .serializers import PongSerializer, RPSSerializer
+from .serializers import PongSerializer, RPSSerializer, RPSSerializerHistory, PongSerializerHistory
 
 class PongViewSet(viewsets.ModelViewSet):
     queryset = PongGame.objects.all()
     serializer_class = PongSerializer
 
-
-class RPSViewSet(viewsets.ModelViewSet):
-    serializer_class = RPSSerializer
-    queryset = RPSGame.objects.all()
+    def get_serializer_class(self):
+        if self.action == 'history':
+            return PongSerializerHistory
+        return super().get_serializer_class()
 
     @action(detail=True, methods=['get'], url_path='history')
     def history(self, request, pk=None):
+        serializer = PongSerializerHistory
         user = get_object_or_404(Users, intra_id=pk)
-        primary_key = user.user_id
         if user is None:
             return JsonResponse({"message": "User not found"}, status=404)
-        games = RPSGame.objects.filter(Q(player1=user) | Q(player2=user))
+        primary_key = user.user_id
+        # type_filter = request.query_params.get('type', "")
+        games = PongGame.objects.filter(Q(winner=user) | Q(loser=user))
+        # games = games.filter(type=type_filter)
         serializer = self.get_serializer(games, many=True, context={'request': request})
         return Response(serializer.data)
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['request'] = self.request
-        return context
 
-#날짜, 상대, 승패, 선택
+class RPSViewSet(viewsets.ModelViewSet):
+    queryset = RPSGame.objects.all()
+    serializer_class = RPSSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'history':
+            return RPSSerializerHistory
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=['get'], url_path='history')
+    def history(self, request, pk=None):
+        serializer = RPSSerializerHistory
+        user = get_object_or_404(Users, intra_id=pk)
+        if user is None:
+            return JsonResponse({"message": "User not found"}, status=404)
+        primary_key = user.user_id
+        games = RPSGame.objects.filter(Q(player1=user) | Q(player2=user))
+        serializer = self.get_serializer(games, many=True, context={'request': request})
+        return Response(serializer.data)
